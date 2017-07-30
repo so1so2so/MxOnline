49fb49b3f6b690405ba6926c663a712ca514f994
so1so2so <1037930435@qq.com>
2017-07-30 22:50:30 +0800

    public class page

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -16,9 +16,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url,include
-from .views import CourseListView,CourseDetaiView
+from .views import CourseListView,CourseDetaiView, CourseInfoView, CommentsView, AddComentsView
 urlpatterns = [
       url(r'^list/$', CourseListView.as_view(), name='course_list'),
       url(r'^detail/(?P<org_id>\d+)$', CourseDetaiView.as_view(), name='course_detail'),
+
+      url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name="course_info"),
+
+        #  课程评论
+      url(r'^comment/(?P<course_id>\d+)/$', CommentsView.as_view(), name="course_comments"),
+
+        # 添加课程评论
+      url(r'^add_comment/$', AddComentsView.as_view(), name="add_comment")
 
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,14 +1,13 @@
 # !/usr/bin/env python
 # _*_ coding:utf-8 _*_
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 from django.views.generic.base import View
 # Create your views here.
 # 分页函数
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
-from .models import Course
+from .models import Course,CourseResource
 from operation.models import UserFavorite
-from operation.models import UserCourse
-
+from operation.models import UserCourse,CourseComments
 class CourseListView(View):
     def get(self, request):
         all_course = Course.objects.all().order_by("-add_time")
@@ -65,3 +64,67 @@ class CourseDetaiView(View):
             'has_fav_org':has_fav_org,
             'relate_coures':relate_coures,
         })
+
+
+class CourseInfoView(View):
+    """
+    课程章节信息
+    """
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        course.students += 1
+        course.save()
+        #查询用户是否已经关联了该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
+        user_cousers = UserCourse.objects.filter(course=course)
+        user_ids = [user_couser.user.id for user_couser in user_cousers]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        #取出所有课程id
+        course_ids = [user_couser.course.id for user_couser in all_user_courses]
+        #获取学过该用户学过其他的所有课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by("-click_nums")[:5]
+        all_resources = CourseResource.objects.filter(course=course)
+        return render(request, "course-video.html", {
+            "course":course,
+            "course_resources":all_resources,
+            "relate_courses":relate_courses
+        })
+
+class CommentsView(View):
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        all_comments = CourseComments.objects.all().order_by("-id")
+        return render(request, "course-comment.html", {
+            "course":course,
+            "course_resources":all_resources,
+            "all_comments":all_comments
+
+        })
+
+
+class AddComentsView(View):
+    """
+    用户添加课程评论
+    """
+    def post(self, request):
+        if not request.user.is_authenticated():
+            #判断用户登录状态
+            return HttpResponse('{"status":"fail", "msg":"用户未登录"}', content_type='application/json')
+
+        course_id = request.POST.get("course_id", 0)
+        comments = request.POST.get("comments", "")
+        if course_id >0 and comments:
+            course_comments = CourseComments()
+            course = Course.objects.get(id=int(course_id))
+            course_comments.course = course
+            course_comments.comments = comments
+            course_comments.user = request.user
+            course_comments.save()
+            return HttpResponse('{"status":"success", "msg":"添加成功"}', content_type='application/json')
+        else:
+            return HttpResponse('{"status":"fail", "msg":"添加失败"}', content_type='application/json')
