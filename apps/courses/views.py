89a03e15a74c4c1fdc6bc78d8e313db6357da90b
so1so2so <1037930435@qq.com>
2017-07-30 22:35:17 +0800

    public class page

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -6,11 +6,12 @@ from django.views.generic.base import View
 # 分页函数
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from .models import Course
+from operation.models import UserFavorite
 from operation.models import UserCourse
 
 class CourseListView(View):
     def get(self, request):
-        all_course = Course.objects.all()
+        all_course = Course.objects.all().order_by("-add_time")
         # 课程排序
         sort = request.GET.get('sort')
         if sort:
@@ -19,6 +20,7 @@ class CourseListView(View):
             elif sort == 'hot':
                 all_course=all_course.order_by('-click_nums')
         hot_orgs = all_course.order_by("-click_nums")[:3]
+        # 课程分页
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
@@ -36,14 +38,30 @@ class CourseListView(View):
 
 
 class CourseDetaiView(View):
-    def get(self, request,org_id):
-        course = Course.objects.filter(id=int(org_id))
-        all_user = UserCourse.objects.filter(course=course)
-        user = all_user.userprofile_set.all()
-        # print all_user
-        a = request.path
+    def get(self, request, org_id):
+
+        course = Course.objects.get(id=int(org_id))
+        # 增加课程点击数
+        course.click_nums += 1
+        course.save()
+        # 是否收藏课程
+        has_fav_course = False
+        # 是否收藏机构
+        has_fav_org = False
+        # 判断用户登录
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.id, fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org.id, fav_type=2):
+                has_fav_org = True
+        tag = course.tag
+        if tag:
+            relate_coures = Course.objects.filter(tag=tag)[:3]
+        else:
+            relate_coures = []
         return render(request, 'course-detai.html',{
-            'all_course': course,
-            'a':a,
-            'all_user':all_user,
+            'course': course,
+            'has_fav_course':has_fav_course,
+            'has_fav_org':has_fav_org,
+            'relate_coures':relate_coures,
         })
